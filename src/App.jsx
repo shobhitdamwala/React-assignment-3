44739a66111eb570a5667cd66f590effb8181575
shobhit@2003 <shobhitdamwala@gmail.com>
2024-11-14 17:45:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+import TaskManager from './components/TaskManager';
+function App() {
+  return (
+    <div className="App">
+      <h1>Task Management</h1>
+      <TaskManager />
+    </div>
+  );
+}
+export default App;
